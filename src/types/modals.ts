3df801c586231b98fdbd4ee172b586ddf6a4138f
Cradlock <ajdarasirov0@gmail.com>
2026-04-30 21:30:26 +0600

    add models,notify

--- src/types/modals.ts
+++ src/types/modals.ts
@@ -1,5 +1,14 @@
+import type { Dispatch, ReactNode, SetStateAction } from "react";
 
 
+export interface Modal { 
+  id: number,
+  child: ReactNode 
+};
 
+export interface ModalsContextType{
+  modals: Modal[],
+  setModals: Dispatch<SetStateAction<Modal[]>>,
+};
 
 
