1286d61e74ee96bfb76b43cf19a8c311c43f6162
cradlock <pefig8@gmail.com>
2026-08-29 22:04:30 +0600

    fix bug web support

--- lib/features/auth/services/main.dart
+++ lib/features/auth/services/main.dart
@@ -20,10 +20,11 @@ class AuthService {
   final String _botRedirectUri = envKey("TELEGRAM_REDIRECT_URI");
     
   Future<WrResponse<TelegramAuthResponse>> loginWithTelegram(BuildContext context) async {
-    String idToken = await getTelegramId(
-      clientId: _botClientId, redirectUri: _botRedirectUri);
+//    String idToken = await getTelegramId(
+//      clientId: _botClientId, redirectUri: _botRedirectUri);
     
-    debugPrint(idToken);
+// debugPrint(idToken);
+    
 
     TelegramAuthRequest data = TelegramAuthRequest(idToken: "");
     return  _api.post<TelegramAuthResponse>("auth/telegram",data: data, converter: TelegramAuthResponse.converter);
