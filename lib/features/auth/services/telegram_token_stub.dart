99e5cce36f2dd6e47ef2126c96134f60b428a5dd
cradlock <pefig8@gmail.com>
2026-08-29 21:26:59 +0600

    fix bug web support

--- lib/features/auth/services/telegram_token_stub.dart
+++ lib/features/auth/services/telegram_token_stub.dart
@@ -1,11 +1,14 @@
 import 'package:app_front/features/auth/domain/telegram_errors.dart';
+import 'package:flutter/material.dart';
 
 
 Future<String> getTelegramId({
   required String clientId,
   required String redirectUri
-}) async => throw AuthPlatformNotSupported(requiredPlatform: "Only web or mobile");
-
+}) async {
+  debugPrint('Hello, you are not on mobile or browser');
+  throw AuthPlatformNotSupported(requiredPlatform: "Only web or mobile");
+}
 
 
 
